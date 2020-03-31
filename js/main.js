e09a65546bc78491ada3cbc2ea4fd2852116aff1
Nicolo <nicolorizzi1996@gmail.com>
2020-03-31 14:55:18 +0200

    INIT

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// GENERO UN VETTORE CON ELEMENTI GIA' ESISTENTI
+
+var vectorSurname = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+
+// RICEVO IN INPUT IL VALORE ACQUISITO
+
+var cognome = document.getElementById('cognome').value;
+console.log(cognome);
+
